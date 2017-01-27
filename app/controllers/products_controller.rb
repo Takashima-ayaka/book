5503b4ad3db3eb3075ebb7b214407c1f54434645
student <monkastudent@gmail.com>
2017-01-27 01:33:21 +0000

    商品一覧の絞り込み検索の変更対応

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   def index
+    @product = Product.ransack(params[:q])
+    products = @product.result.visible
     @books = Book.visible.all
     @musics = Music.visible.all
     render layout: 'front'
