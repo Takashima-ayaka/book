49bc72ed1953ec68956da8ab4d41b167c66e3939
student <monkastudent@gmail.com>
2017-01-30 03:01:10 +0000

    修正

--- app/controllers/orders_management_controller.rb
+++ app/controllers/orders_management_controller.rb
@@ -2,6 +2,17 @@ class OrdersManagementController < ApplicationController
     before_action :authenticate_user!
     before_action :set_order, only: [:edit, :confirm_payment, :deliver, :async_confirm_payment, :async_deliver]
     
+      
+    def async_confirm_payment
+        @order.confirm_payment!
+        render template: "orders_management/async_process"
+    end
+    
+    def async_deliver
+        @order.deliver!
+        render template: "orders_management/async_process"
+    end
+    
     def index
         conditions = params[:q] || { status_in: Order.statuses.values }
         @order = Order.ransack(conditions)
@@ -46,18 +57,7 @@ class OrdersManagementController < ApplicationController
     #         end
     #     end
     # end
-    
-    
-    def async_confirm_payment
-        @order.confirm_payment!
-        render template: "orders_management/async_process"
-    end
-    
-    def async_deliver
-        @order.deliver!
-        render template: "orders_management/async_process"
-    end
-    
+  
 private
 
     def set_order
